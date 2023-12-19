fa98f0db66bb3f94e65a616bc1dcea723e1795fa
Daiyrbek <daiyrbekakparaliev0@gmail.com>
2023-12-19 14:26:39 +0600

    text

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// let product = [
+//     {
+//         id: 1,
+//         price: 11600,
+//         title: "table",
+//         fon: "add",
+//     },
+//     {
+//         id: 2,
+//         price: 11600,
+//         title: "table",
+//         fon: "wak",
+//     }
+// ]
+
+// let product_div = document.querySelector(".two-img")
+
+// let productPost = product.map((item) => {
+//     return `
+//     <div class="two-img-image">
+//        <h5>${item.price}</h5>
+//        <p>${item.title}</p>
+//        <span>11.990 руб</span>
+//     </div>
+//     `
+// })
+// product_div.innerHTML = productPost.join("")
+
+
+// let a = 3;
+// console.log(a);
